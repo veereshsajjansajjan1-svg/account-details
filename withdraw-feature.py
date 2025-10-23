4855417e8bd68b6f346b3fe87e072f61c45440cf
veeresh <veeresh@123>
2025-10-23 17:13:47 +0530

    save to branch 1

--- withdraw-feature.py
+++ withdraw-feature.py
@@ -6,3 +6,17 @@ balance += deposit
 print(f"Initial Balance: ₹{balance - deposit}")
 print(f"Deposit: ₹{deposit}")
 print(f"New Balance after deposit: ₹{balance}")
+
+
+balance = float(input("Enter your initial balance: "))
+deposit = float(input("Enter deposit amount: "))
+balance += deposit
+
+withdraw = float(input("Enter withdrawal amount: "))
+balance -= withdraw
+
+print(f"Initial Balance: ₹{balance + withdraw - deposit}")
+print(f"Deposit: ₹{deposit}")
+print(f"New Balance after deposit: ₹{balance + withdraw}")
+print(f"Withdraw: ₹{withdraw}")
+print(f"Final Balance: ₹{balance}")
